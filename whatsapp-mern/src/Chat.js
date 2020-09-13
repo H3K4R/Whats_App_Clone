e73c20ae514b467d70199c9e81ca8e90d0719529
Swarnava Basu <basuswarnava2@gmail.com>
2020-09-13 13:42:37 +0530

    FrontEnd Done

--- whatsapp-mern/src/Chat.js
+++ whatsapp-mern/src/Chat.js
@@ -1,10 +1,66 @@
 import React from 'react'
+import {Avatar, IconButton} from "@material-ui/core"
 import './Chat.css'
+import { MoreVert, SearchOutlined, AttachFile, Message } from '@material-ui/icons'
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon"
+import MicIcon from "@material-ui/icons/Mic"
+
+
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>Hello Chat</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton><SearchOutlined /></IconButton>
+                    <IconButton><AttachFile /></IconButton>
+                    <IconButton><MoreVert /></IconButton>
+
+                </div>
+            </div>
+            <div className="chat__body">
+                <p className="chat__message">
+                <span className="chat__name">Swarnava</span>
+                     This is a message
+                <span className="chat__timestamp">
+                    {new Date().toUTCString()}
+                </span> 
+                </p>
+
+                <p className="chat__message chat__reciever">
+                <span className="chat__name">Swarnava</span>
+                     This is a message
+                <span className="chat__timestamp">
+                    {new Date().toUTCString()}
+                </span> 
+                </p>
+
+                <p className="chat__message">
+                <span className="chat__name">Swarnava</span>
+                     This is a message
+                <span className="chat__timestamp">
+                    {new Date().toUTCString()}
+                </span> 
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input
+                    placeholder="Type a message"
+                    type="text" />
+                    <button type="submit">Send a Message</button>
+                </form>
+                <MicIcon />
+
+            </div>
             
         </div>
     )
